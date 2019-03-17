4fc2de2e809149b9a52e06334ee9b7bd35c518c5
Travezripley <theripleys@gmail.com>
2019-03-16 17:37:40 -0700

    adds initial project files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,29 @@
 // import files and packages up here
+const data = require("./data.json");
+const express = require("express");
 
+const morgan = require("morgan");
 
+var path = require('path');
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+
+app.use(morgan('dev'));
+
+//app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/data', (req, res) => {
+    res.json(data);
+});
+
+//var server = app.listen(3000, function() {
+    //console.log('listening on port 3000');
 
 // finally export the express application
 module.exports = app;
